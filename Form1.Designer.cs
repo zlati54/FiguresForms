41e8b6b5e8ba8c5e6361c702574e04d356010aa9
zlatozar <zlatozarpoptolev@gmail.com>
2022-12-09 15:10:05 +0200

    Added buttons for spawing objects

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnRectangle = new System.Windows.Forms.Button();
+            this.btnTriangle = new System.Windows.Forms.Button();
+            this.btnCircle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRectangle
+            // 
+            this.btnRectangle.Location = new System.Drawing.Point(71, 388);
+            this.btnRectangle.Name = "btnRectangle";
+            this.btnRectangle.Size = new System.Drawing.Size(75, 23);
+            this.btnRectangle.TabIndex = 0;
+            this.btnRectangle.Text = "rectangle";
+            this.btnRectangle.UseVisualStyleBackColor = true;
+            // 
+            // btnTriangle
+            // 
+            this.btnTriangle.Location = new System.Drawing.Point(217, 395);
+            this.btnTriangle.Name = "btnTriangle";
+            this.btnTriangle.Size = new System.Drawing.Size(75, 23);
+            this.btnTriangle.TabIndex = 1;
+            this.btnTriangle.Text = "triangle";
+            this.btnTriangle.UseVisualStyleBackColor = true;
+            // 
+            // btnCircle
+            // 
+            this.btnCircle.Location = new System.Drawing.Point(366, 394);
+            this.btnCircle.Name = "btnCircle";
+            this.btnCircle.Size = new System.Drawing.Size(75, 23);
+            this.btnCircle.TabIndex = 2;
+            this.btnCircle.Text = "circle";
+            this.btnCircle.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCircle);
+            this.Controls.Add(this.btnTriangle);
+            this.Controls.Add(this.btnRectangle);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button btnRectangle;
+        private Button btnTriangle;
+        private Button btnCircle;
     }
 }
